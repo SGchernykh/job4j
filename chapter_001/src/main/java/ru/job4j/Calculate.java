624a0bcc178a45e5434b58aec0243f2eefe9e3e9
Sergey Chernykh <gtjs2015@gmail.com>
2018-05-14 14:28:31 +0500

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author Sergey Chernykh(chernykh.sergey95@gmail.com)
+*/
+
 public class Calculate {
-	
+	/**
+	* Main
+	*
+	* @param args - args. 
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
