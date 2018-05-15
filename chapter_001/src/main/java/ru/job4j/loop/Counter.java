ed63f1320c1943ecddfd3545f40c461860cbcbe6
Sergey Chernykh <gtjs2015@gmail.com>
2018-05-15 09:41:20 +0500

    Добавление JavaDoc

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -6,6 +6,12 @@ package ru.job4j.loop;
  * @since 0.1
  */
 public class Counter {
+    /**
+     * Сумма четных числе из диапазона.
+     * @param start начало диапазона.
+     * @param finish конец диапазона.
+     * @return результат.
+     */
     public int add(int start, int finish){
         int result = 0;
 
