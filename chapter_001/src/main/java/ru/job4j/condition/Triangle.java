87f6ec745522cda5b191a2b467cd1f81353edcc5
Sergey Chernykh <gtjs2015@gmail.com>
2018-05-18 13:00:29 +0500

    3.3. Вычисление площади треугольника v-2

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -62,7 +62,7 @@ public class Triangle {
      * @return
      */
     private boolean exist(double ab, double ac, double bc) {
-       return  (((ab + ac) > bc) && ((ab + bc) > ac) && ((bc + ac) > ab)) ?  true : false;
+       return  (((ab + ac) > bc) && ((ab + bc) > ac) && ((bc + ac) > ab));
     }
 }
 
