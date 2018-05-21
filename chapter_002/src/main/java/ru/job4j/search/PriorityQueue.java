0b37d7ca6ce049294218c0eb845fc655f68f16ca
Sergey Chernykh <gtjs2015@gmail.com>
2018-05-21 10:33:01 +0500

    2. Очередь с приоритетом на LinkedList

---
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,39 @@
+package ru.job4j.search;
+/**
+ * @author Sergey Chernykh(chernykh.sergey95@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        if (this.tasks.size() == 0) {
+            this.tasks.add(task);
+        } else {
+            int size = this.tasks.size();
+            for (int index = 0; index < size; index++) {
+                if (task.getPriority() < this.tasks.get(index).getPriority()) {
+                    this.tasks.add(index,task);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * The method displays and removes the first element from the list.
+     * @return Displays and removes the first element.
+     */
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.search;
+/**
+ * @author Sergey Chernykh(chernykh.sergey95@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+}
