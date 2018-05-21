ef7d92e54b87760aa817a65653ff4ef7f458a33c
Sergey Chernykh <gtjs2015@gmail.com>
2018-05-22 01:11:42 +0500

    2. Сортировка User с использованием Comparator

--- chapter_002/src/main/java/ru/job4j/map/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/map/SortUser.java
@@ -1,8 +1,6 @@
 package ru.job4j.map;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Sort user.
@@ -24,5 +22,33 @@ public class SortUser {
         }
         return result;
     }
+    public List<User> sortNameLength (List<User> list) {
+        List<User> result = new ArrayList<>();
+        result.addAll(list);
+        result.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getName().length() - o2.getName().length();
+                    }
+                }
+        );
+        return result;
 
+    }
+
+    public List<User> sortByAllFields (List<User> list) {
+        List<User> result = new ArrayList<>();
+        result.addAll(list);
+        result.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        final int rs1 = o1.getName().compareTo(o2.getName());
+                        return rs1 != 0 ? rs1 : Integer.compare(o1.getAge(), o2.getAge());
+                    }
+                }
+        );
+        return result;
+    }
 }
--- chapter_002/src/test/java/ru/job4j/map/SortUserTest.java
+++ chapter_002/src/test/java/ru/job4j/map/SortUserTest.java
@@ -1,10 +1,11 @@
 package ru.job4j.map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 
 import static org.hamcrest.core.Is.is;
@@ -17,25 +18,84 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class SortUserTest {
+    private ArrayList<User> list = new ArrayList<>();
+    @Before
+    public void userList() {
+        User user1 = new User("test1", 20);
+        User user2 = new User("test2222", 19);
+        User user3 = new User("test3", 25);
+        User user4 = new User("test4", 17);
+        User user5 = new User("test44", 14);
+        User user6 = new User("test4", 35);
+        this.list.add(user1);
+        this.list.add(user2);
+        this.list.add(user3);
+        this.list.add(user4);
+        this.list.add(user5);
+        this.list.add(user6);
+
+    }
     @Test
     public void whenListUsersSortedSet() {
         SortUser sortUser = new SortUser();
-        ArrayList<User> list = new ArrayList<>();
         Set<User> result;
         ArrayList<User> expect = new ArrayList<>();
         User user1 = new User("test1", 20);
-        User user2 = new User("test2", 19);
+        User user2 = new User("test2222", 19);
         User user3 = new User("test3", 25);
         User user4 = new User("test4", 17);
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-        list.add(user4);
+        User user5 = new User("test44", 14);
+        User user6 = new User("test4", 35);
+        expect.add(user5);
         expect.add(user4);
         expect.add(user2);
         expect.add(user1);
         expect.add(user3);
-        result = sortUser.sort(list);
+        expect.add(user6);
+        result = sortUser.sort(this.list);
         assertThat(result.toString(), is(expect.toString()));
     }
+
+    @Test
+    public void sortTheListByWordLength() {
+        SortUser sortUser = new SortUser();
+        List<User> result;
+        ArrayList<User> expect = new ArrayList<>();
+        User user1 = new User("test1", 20);
+        User user2 = new User("test2222", 19);
+        User user3 = new User("test3", 25);
+        User user4 = new User("test4", 17);
+        User user5 = new User("test44", 14);
+        User user6 = new User("test4", 35);
+        expect.add(user1);
+        expect.add(user3);
+        expect.add(user4);
+        expect.add(user6);
+        expect.add(user5);
+        expect.add(user2);
+        result = sortUser.sortNameLength(this.list);
+        assertThat(result.toString(), is(expect.toString()));
+    }
+
+    @Test
+    public void whenSortedListByAllFields() {
+        SortUser sortUser = new SortUser();
+        List<User> result;
+        ArrayList<User> expect = new ArrayList<>();
+        User user1 = new User("test1", 20);
+        User user2 = new User("test2222", 19);
+        User user3 = new User("test3", 25);
+        User user4 = new User("test4", 17);
+        User user5 = new User("test44", 14);
+        User user6 = new User("test4", 35);
+        expect.add(user1);
+        expect.add(user2);
+        expect.add(user3);
+        expect.add(user4);
+        expect.add(user6);
+        expect.add(user5);
+        result = sortUser.sortByAllFields(this.list);
+        assertThat(result.toString(), is(expect.toString()));
+    }
+
 }
