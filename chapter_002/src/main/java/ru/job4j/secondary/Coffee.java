349583b22cf3b4365e77d1f983da5eea3f125e5b
Sergey Chernykh <gtjs2015@gmail.com>
2018-05-23 11:54:02 +0500

    Кофемашина.

---
+++ chapter_002/src/main/java/ru/job4j/secondary/Coffee.java
@@ -0,0 +1,46 @@
+package ru.job4j.secondary;
+/**
+ * @author Sergey Chernykh(chernykh.sergey95@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Coffee {
+    private int[] coins = {10, 5, 2, 1};
+
+    /**
+     *
+     * @param value купюра.
+     * @param price цена кофе.
+     * @return сдача.
+     */
+    int[] changes(int value, int price) {
+        int[] result = new int[this.coins.length];
+        int residue = value - price;
+        int interim = residue;
+        int iter = 0;
+        int count = 0;
+        for (int coin = 0; coin < this.coins.length; coin++) {
+            if (residue % this.coins[coin] != 0) {
+                interim /= this.coins[coin];
+                result[coin] = interim;
+                residue -= this.coins[coin] * result[coin];
+                count += interim;
+            } else {
+                result[coin] = residue / this.coins[coin];
+                count += result[coin];
+                break;
+            }
+        }
+        int[] amount = new int[count];
+        for (int coin = 0; coin < this.coins.length; coin++) {
+            if (iter == count) {
+                break;
+            }
+            for (int index = 0; index < result[coin]; index++) {
+                amount[iter] = this.coins[coin];
+                iter++;
+            }
+        }
+        return amount;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/secondary/CoffeeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.secondary;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Sergey Chernykh(chernykh.sergey95@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CoffeeTest {
+    @Test
+    public void whenInCoffeeMachineIntroduce50ForCoffee35Result105() {
+        Coffee coffee = new Coffee();
+        assertThat(coffee.changes(50, 35), is(new int[]{10,5}));
+    }
+
+    @Test
+    public void whenInCoffeeMachineIntroduce100ForCoffee35Result1010101010105() {
+        Coffee coffee = new Coffee();
+        assertThat(coffee.changes(100, 35), is(new int[]{10, 10, 10, 10, 10, 10, 5}));
+    }
+}
