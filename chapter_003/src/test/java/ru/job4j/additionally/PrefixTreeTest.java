dd6e221390c65c1522851b35170e714e9781173b
Sergey Chernykh <gtjs2015@gmail.com>
2018-06-08 18:09:12 +0500

    ref

--- chapter_003/src/test/java/ru/job4j/additionally/PrefixTreeTest.java
+++ chapter_003/src/test/java/ru/job4j/additionally/PrefixTreeTest.java
@@ -25,4 +25,16 @@ public class PrefixTreeTest {
         Set<Integer> example = new HashSet<>(Arrays.asList(0, 18, 2, 15));
         assertThat(tree.getIndexes4Word("ma"), is(example));
     }
+
+    @Test
+    public void whenInFileFourMathEqualsIndexOf() {
+        Set<Integer> example = new HashSet<>();
+        PrefixTree tree = new PrefixTree();
+        String test = "mama papa fediama mary";
+        tree.put(test);
+        System.out.println();
+        example.add(test.indexOf("ry"));
+        assertThat(tree.getIndexes4Word("ry"), is(example));
+    }
+
 }
