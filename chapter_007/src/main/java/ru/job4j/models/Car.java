0759702c6134d0d87df7743a9c6a41ed51e7c358
Sergey Chernykh <gtjs2015@gmail.com>
2018-07-03 11:17:46 +0500

    Реализовать модели и связи для задание SQL хранилище машин. [#4744]

---
+++ chapter_007/src/main/java/ru/job4j/models/Car.java
@@ -0,0 +1,49 @@
+package ru.job4j.models;
+
+public class Car {
+    private int id;
+    private String name;
+    private CarBody carBody;
+    private Engine engine;
+    private Transmission transmission;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CarBody getCarBody() {
+        return carBody;
+    }
+
+    public void setCarBody(CarBody carBody) {
+        this.carBody = carBody;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Transmission getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+}
